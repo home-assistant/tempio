ae7e011b3ea58cef6341fc7b34c580766d5b718b
Ludeeus <ludeeus@ludeeus.dev>
2021-09-27 12:20:34 +0000

    Add version and doc link to help

--- main.go
+++ main.go
@@ -7,11 +7,22 @@ import (
 	"log"
 )
 
+var TempioVersion string
+
 func main() {
 	var config *map[string]interface{}
 	configFile := flag.String("conf", "", "Config json file, can be omitted if used in a pipe")
 	templateFile := flag.String("template", "", "Template file")
 	outFile := flag.String("out", "", "Output file, if not defined output will be to console")
+
+	flag.Usage = func() {
+		writer := flag.CommandLine.Output()
+
+		fmt.Fprintf(writer, "Version: %s\n", TempioVersion)
+		fmt.Fprintf(writer, "Documentation: https://github.com/home-assistant/tempio\n\n")
+
+		flag.PrintDefaults()
+	}
 
 	// parse command lines
 	flag.Parse()
